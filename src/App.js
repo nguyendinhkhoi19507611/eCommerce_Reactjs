63a532808c5356e976f36e13fdbc30daf8d5d577
nguyendinhkhoi19507611 <khoinguyenh6@gmail.com>
2024-11-09 02:44:01 +0700

    Giao diện HomePage, Hiển thị Banner , Hiển thị Các Sản Phẩm Gợi Ý và Đặc Trưng, Hiển thị Sản Phẩm Mới, Hiển thị Bài Viết Blog,

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment, useEffect, useState } from 'react';
+import './css/App.css';
 import { ToastContainer } from 'react-toastify';
 import Header from './container/Header/Header';
 import Footer from './container/Footer/Footer';
-
+import HomePage from './container/Home/HomePage';
 import {
   BrowserRouter as Router,
   Switch,
@@ -18,7 +19,7 @@ function App() {
         <div className="App">
           <Route exact path="/">
             <Header />
-            
+            <HomePage />
             <Footer />
           </Route>
           <ToastContainer
