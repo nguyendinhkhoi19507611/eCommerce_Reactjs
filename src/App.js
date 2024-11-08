45387d8d1d941d98b3c68c31bf81980aed49afa2
nguyendinhkhoi19507611 <khoinguyenh6@gmail.com>
2024-11-09 03:03:12 +0700

    DetailProductPage , giao diện Thông Tin Chi Tiết Sản Phẩm,  Mô Tả và Đánh Giá, Sản Phẩm Gợi Ý (ProductFeature)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './container/Header/Header';
 import Footer from './container/Footer/Footer';
 import HomePage from './container/Home/HomePage';
 import ShopPage from './container/Shop/ShopPage';
+import DetailProductPage from './container/DetailProduct/DetailProductPage';
 import {
   BrowserRouter as Router,
   Switch,
@@ -26,6 +27,11 @@ function App() {
           <Route path="/shop">
             <Header />
             <ShopPage />
+            <Footer />
+          </Route>
+          <Route path="/detail-product/:id">
+            <Header />
+            <DetailProductPage />
             <Footer />
           </Route>
           <ToastContainer
